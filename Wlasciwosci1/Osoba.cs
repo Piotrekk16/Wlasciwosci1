4bcfafba0589bec752c329c35b246688a6b4d353
Piotrekk85 <piotr.jastr@gmail.com>
2017-01-15 14:45:03 +0100

    Add files via upload

---
+++ Wlasciwosci1/Osoba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wlasciwosci1
+{
+    class Osoba
+    {
+        private string imie;
+        private int wiek;
+
+        public void UstawWiek(int wiek) { this.wiek = wiek; }
+        public int PobierzWiek() { return wiek; }
+
+        public void UstawImie(string imie) { this.imie = imie; }
+        public string PobierzImie() { return imie; }
+    }
+}
---
+++ Wlasciwosci1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wlasciwosci1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba osoba1 = new Osoba();
+            osoba1.UstawWiek(21); //długo
+
+            osoba1.wiek = 21; //niedostępne
+
+        }
+    }
+}
